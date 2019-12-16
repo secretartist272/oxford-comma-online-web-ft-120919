7399f6512df3361d75e5b8021744f99921aa1716
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-16 14:52:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     return array.join
   
-  elsif array.length >= 2 
+  elsif array.length == 2 
     return array.join(" and ")
   
   else array.length >= 3
