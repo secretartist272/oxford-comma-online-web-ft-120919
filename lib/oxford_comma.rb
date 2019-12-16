2ffa245886aabf9bd03331eb802d3dfd0ccfe06f
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-16 14:59:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,31 @@
 def oxford_comma(array)
-  ["kiwi"].join
+  if array.length == 1
+    return array.join
+  
+  elsif array.length == 2 
+    return array.join(" and ")
+  
+  else array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+    
+  end
+  
 end
+  
 
 
-def oxford_comma(array)
-  ["kiwi", "durian"].join(" and ")
-end
+# def oxford_comma(array)
+# case array
+ 
+# when array.length == l 
+#   return array.join
+  
+#   when array.length == 2 
+#     return array.join(" and ")
+  
+#   when array.length >= 3
+#     array[-1] = " and #{array[-1]}"
+#     return array.join(", ")
+#   end
+# end
