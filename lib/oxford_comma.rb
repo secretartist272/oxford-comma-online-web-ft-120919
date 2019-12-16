5aaee921075987bb8ac99b5ff4997b470c3b0065
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-16 14:17:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ def oxford_comma(array)
     return array.join(" and ")
   
   else array.length >= 3
-    
-    return array.join(", ")array[-1] = " and #{array[-1]}"
+    return array.join(", ")
+    array[-1] = " and #{array[-1]}"
   end
 end
   
